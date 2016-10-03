6bd1bbadddb6d7e0b319041595fff5624dfe6d4f
Andres Moschini <andresmoschini@gmail.com>
2016-10-03 09:31:32 -0300

    Add SMTP/MailKit Example

---
+++ SmptClient/SmptClient/DataInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmptClient
+{
+    public class DataInput
+    {
+        public ServerConfiguration server { get; set; }
+        public DataItem common { get; set; }
+        public DataItem[] items { get; set; }
+    }
+
+    public class ServerConfiguration
+    {
+        public string host { get; set; }
+        public int port { get; set; }
+        public string username { get; set; }
+        public string apikey { get; set; }
+    }
+
+    public class DataItem
+    {
+        public string fromEmail { get; set; }
+        public string fromName { get; set; }
+        public string subject { get; set; }
+        public string htmlFile { get; set; }
+        public string textFile { get; set; }
+        public string toEmail { get; set; }
+        public string toName { get; set; }
+        public string[] attachmentFiles { get; set; } = new string[0];
+    }
+}
---
+++ SmptClient/SmptClient/Program.cs
@@ -0,0 +1,86 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmptClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading data from data.json...");
+
+            var serialized = File.ReadAllText("data.json");
+
+            Console.WriteLine("Parsing data...");
+
+            var data = JsonConvert.DeserializeObject<DataInput>(serialized);
+
+            Console.WriteLine("Loading common HTML and Text files...");
+
+            var commonHtml = data.common.htmlFile != null ? File.ReadAllText(data.common.htmlFile) : null;
+            var commonText = data.common.textFile != null ? File.ReadAllText(data.common.textFile) : null;
+
+            foreach (var item in data.items)
+            {
+                Console.WriteLine("Processing " + item.toEmail);
+
+                try
+                {
+                    Console.WriteLine("Building body...");
+
+                    var builder = new BodyBuilder();
+                    builder.HtmlBody = item.htmlFile != null ? File.ReadAllText(item.htmlFile) : commonHtml;
+                    builder.TextBody = item.textFile != null ? File.ReadAllText(item.textFile) : commonText;
+
+                    foreach (var attachmentFile in data.common.attachmentFiles.Union(item.attachmentFiles))
+                    {
+                        builder.Attachments.Add(attachmentFile);
+                    }
+
+                    Console.WriteLine("Building message...");
+
+                    var message = new MimeMessage(
+                        from: new[] { new MailboxAddress(
+                            item.fromName ?? data.common.fromName,
+                            item.fromEmail ?? data.common.fromEmail)
+                        },
+                        to: new[] { new MailboxAddress(
+                            item.toName ?? data.common.toName,
+                            item.toEmail ?? data.common.toEmail)
+                        },
+                        subject: item.subject ?? data.common.subject,
+                        body: builder.ToMessageBody());
+
+                    Console.WriteLine("Sending...");
+
+                    using (var client = new SmtpClient())
+                    {
+                        client.Connect(data.server.host, data.server.port, false);
+                        client.Authenticate(data.server.username, data.server.apikey);
+                        client.Send(message);
+                        client.Disconnect(true);
+                    }
+
+                    Console.WriteLine("Sent!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR");
+                    Console.WriteLine(e);
+                    Console.WriteLine();
+                }
+                System.Threading.Thread.Sleep(20);
+            }
+
+            Console.WriteLine("Press ENTER to continue . . .");
+            Console.ReadLine();
+        }
+    }
+}
