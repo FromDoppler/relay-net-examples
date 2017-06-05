35b4059180b69f3082aba850266b2cab425f1f72
Andres Moschini <andresmoschini@gmail.com>
2017-06-05 10:58:11 -0300

    DR-1121 - Send email using API

---
+++ ApiClient/ApiClient/DataInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class DataInput
+    {
+        public ServerConfiguration server { get; set; }
+        public DataItem common { get; set; }
+        public DataItem[] items { get; set; }
+    }
+
+    public class ServerConfiguration
+    {
+        public string sendMessageUrlTemplate { get; set; }
+        public string apikey { get; set; }
+        public int accountId { get; set; }
+        public string accountName { get; set; }
+    }
+
+    public class DataItem
+    {
+        public string fromEmail { get; set; }
+        public string fromName { get; set; }
+        public string subject { get; set; }
+        public string htmlFile { get; set; }
+        public string textFile { get; set; }
+        public string toEmail { get; set; }
+        public string toName { get; set; }
+        public string[] attachments { get; set; } = new string[0];
+    }
+}
