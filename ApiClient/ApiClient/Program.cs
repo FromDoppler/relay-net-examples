ba91d5c64cc6654185597bccffac0aca6999b8d6
Andres Moschini <andresmoschini@gmail.com>
2017-06-05 09:49:16 -0300

    DR-1121 - Add new empty project for API client

---
+++ ApiClient/ApiClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult(); // to avoid exceptions being wrapped into AggregateException
+        }
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Press ENTER to continue . . .");
+            Console.ReadLine();
+        }
+    }
+}
